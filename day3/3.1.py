466b65dcae736353317f808007352bc5efbe34d2
tannu328 <tanvitaraviya28@gmail.com>
2025-03-21 16:30:52 +0530

    3.1.py

---
+++ day3/3.1.py
@@ -0,0 +1,11 @@
+import math 
+def calculate(x1,y1,x2,y2):
+    distance = math.sqrt ((x2 - x1)**2 + (y2 - y1)**2)
+    return distance 
+x1 = float(input("enter value x1"))
+y1 = float(input("enter value y1"))
+x2 = float(input("enter value x2"))
+y2 = float(input("enter value y2"))
+
+distance = calculate(x1,y1,y2,x2)
+print (f"the length of the line :{distance}")
